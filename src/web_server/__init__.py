ec5af9c4560e426d018c40cdb1e798af6f26c610
Jonathon Roscoe <36338176+jroscoe5@users.noreply.github.com>
2021-10-02 14:36:16 -0700

    Added webserver auth template

---
+++ src/web_server/__init__.py
@@ -0,0 +1,24 @@
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+load_dotenv('.env')
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY']              = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+    db.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
